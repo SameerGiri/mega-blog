9b1d1dc70ea2c2e07cca85174771c1c0409435ff
Sameer Giri <92969034+SameerGiri@users.noreply.github.com>
2025-05-14 12:52:25 +0530

    Initial commit

---
+++ src/store/components/Header/LogOutBtn.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { useDispatch } from 'react-redux'
+import authService from '../../../appwrite/config'
+import { logout } from '../../store/authSlice'
+function LogOutBtn() {
+  return (
+    <div>LogOutBtn</div>
+  )
+}
+
+export default LogOutBtn
