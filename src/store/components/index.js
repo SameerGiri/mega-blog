16f38c50c174b238b16276b20d41df02ea1265c5
Sameer Giri <92969034+SameerGiri@users.noreply.github.com>
2025-06-09 13:23:16 +0530

    nav-bar

--- src/store/components/index.js
+++ src/store/components/index.js
@@ -1,7 +1,6 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
-
-export  {
-    Header,
-    Footer
-}
+import Container from "./container/Container";
+import LogOutBtn from "./Header/LogOutBtn";
+import Logo from "./Logo";
+export { Header, Footer, Container, Logo ,LogOutBtn};
