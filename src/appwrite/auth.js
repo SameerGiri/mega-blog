cabae6d84533b768a1fcdd80c9813b35dc5d8335
Sameer Giri <92969034+SameerGiri@users.noreply.github.com>
2025-07-01 19:41:43 +0530

    update

--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -21,7 +21,7 @@ export class AuthService {
       );
       if (userAccount) {
         //call another method
-       return this.login({email, password});
+        return this.login({ email, password });
       } else {
         return userAccount;
       }
@@ -30,7 +30,7 @@ export class AuthService {
     }
   }
 
-  async login({email, password}) {
+  async login({ email, password }) {
     try {
       return await this.account.createSession(email, password);
     } catch (error) {
